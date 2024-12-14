fd8994e19ce10e5ff88da8b363873b7d18eb6c1c
masha1224 <maria.sgrbv@gmail.com>
2024-12-14 11:41:00 +0100

    Initial commit: Added index.html and script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+ function AddMovie() {
+            var movieTitle = document.getElementById("inputTitle").value;
+            var movieYear = document.getElementById("inputYear").value;
+            var movieActors = document.getElementById("inputActors").value;
+
+            if (movieTitle === "" || movieYear === "" || movieActors === "") {
+                alert("Nie wszytskie pola sa wypelnione");
+                return;
+            }
+
+            var li = document.createElement("li");
+            li.innerHTML = "<input type= 'checkbox'> " + movieTitle + ", " + movieYear + ", " + movieActors;
+            document.getElementById("moviesList").appendChild(li);
+
+            document.getElementById("inputTitle").value = "";
+            document.getElementById("inputYear").value = "";
+            document.getElementById("inputActors").value = "";
+
+        }
+
+        function DeleteMovie() {
+            var checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+            for (var i = 0; i < checkedBoxes.length; i++) {
+                document.getElementById("moviesList").removeChild(checkedBoxes[i].parentNode)
+            }
+        }
